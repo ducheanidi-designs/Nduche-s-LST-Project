fae51a010fc46ca27a39317b23f2b937f3e1241f
ducheanidi-designs <ducheanidi@gmail.com>
2026-08-14 16:15:36 +0100

    Game progress and resolutions

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,23 +1,20 @@
-
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float maxHealth;
     [SerializeField] private float currentHealth;
     
-   
-   
-
-    [SerializeField] private PlayerMov player;
-    
+    [SerializeField] private Image healthSlider;
+       
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
        currentHealth = maxHealth; 
-
          
     }
 
@@ -25,7 +22,12 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHealth -= damage;
 
+        healthSlider.fillAmount = currentHealth/maxHealth;
 
+        if (currentHealth <= 0f)
+        {
+            Debug.Log("Player dead");
+        }
 
        
     }
